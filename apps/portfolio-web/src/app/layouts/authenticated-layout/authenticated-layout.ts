f891d60e6bcec05e0ab8416682107e577cc6450f
Charlie Francis <charlie.francis@nrla.org.uk>
2025-11-19 10:43:08 +0000

    feat: show logged in name in header

--- apps/portfolio-web/src/app/layouts/authenticated-layout/authenticated-layout.ts
+++ apps/portfolio-web/src/app/layouts/authenticated-layout/authenticated-layout.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-authenticated-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, AsyncPipe],
   templateUrl: './authenticated-layout.html',
   styleUrl: './authenticated-layout.css',
 })
-export class AuthenticatedLayout {}
+export class AuthenticatedLayout {
+  private authService = inject(AuthService);
+  public user$ = this.authService.user$;
+}
